0d975f332def4fd5c4427a449a8ef73af9bae171
Arfikait Sirius <Arfikait.Sirius@gmail.com>
2021-03-10 22:52:16 +0900

    Learn Louise. - fnLowerAll(), fnUpperFirst().

--- dir_Girls/Louise/Louise.scala
+++ dir_Girls/Louise/Louise.scala
@@ -4,4 +4,15 @@ object Louise {
      def fnUpperAll( base: String ): String = {
           return base.toUpperCase()
      }
+
+     def fnLowerAll( base: String ): String = {
+          return base.toLowerCase()
+     }
+
+     def fnUpperFirst( base: String ): String = {
+          val baseUpper = base.toUpperCase().head
+          val baseLower = base.toLowerCase().tail
+
+          return s"$baseUpper$baseLower"
+     }
 }
